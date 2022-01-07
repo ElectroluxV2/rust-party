c0e147a26850eaf389072602d13f0cd4e633b264
electroluxv2 <electroluxv2@gmail.com>
2022-01-07 22:47:19 +0100

    Solution

--- print_colored_text_in_console/src/main.rs
+++ print_colored_text_in_console/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", rgb_wrap("Goodbye cruel world", 255, 0, 0));
+}
+
+fn rgb_wrap(text: &str, r: u8, g: u8, b: u8) -> String {
+    format!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, text)
 }
