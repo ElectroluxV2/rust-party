a6a0dc6a60e3c79d9679d3fc8a62cc857856623e
Damian Klein <klein.damian45@gmail.com>
2022-01-08 23:28:48 +0100

    i try

--- eerie_array/src/main.rs
+++ eerie_array/src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+
+enum Operations {
+    A,
+    E,
+    P,
+    M,
+    Q
+}
+
+impl Operations {
+    fn value(&self) -> (char) {
+        match*self {
+            Operations::A => A,
+            Operations::E => E,
+            Operations::P => P,
+            Operations::M => M,
+            Operations::Q => Q
+        }
+    }
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut length = 0;
+    let mut array = vec![0; length];
+}
+
+fn get_input_from_user() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("error not readable input");
+    input.trim().to_string()
 }
