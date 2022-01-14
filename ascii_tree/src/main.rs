42687350f60d970ed81a7e91e98e1c21cd8272db
Damian Klein <klein.damian45@gmail.com>
2022-01-14 23:11:00 +0100

    very good progress today m8

--- ascii_tree/src/main.rs
+++ ascii_tree/src/main.rs
@@ -1,78 +1,81 @@
-enum Color {
-    Red,
-    Orange,
-    Yellow,
-    Green,
-    Blue,
-    Indigo,
-    Violet,
+use std::fmt;
+use Terminal::{Color, Clear, Position};
+
+enum Terminal {
+    Color(u8, u8, u8),
+    Clear(),
+    Position(i8, i8)
 }
 
-impl Color {
-    fn value(&self) -> (u8, u8, u8) {
-        match *self {
-            Color::Red => (255, 65, 54),
-            Color::Orange => (255, 140, 0),
-            Color::Yellow => (255, 255, 0),
-            Color::Green => (0, 255, 0),
-            Color::Blue => (0, 191, 255),
-            Color::Indigo => (75, 0, 130),
-            Color::Violet => (148, 0, 211),
+impl Terminal {
+    fn format(&self, text: &str) -> String {
+        match self{
+            Color(r, g, b) => {
+                format!(
+                "\x1b[38;2;{};{};{}m{}\x1b[0m",
+                r,
+                g,
+                b,
+                text)
+            }
+            Clear() => { format!("\x1b[H\x1b[2J\x1b[3J") }
+            Position(_, _) => {"".to_string()}
         }
     }
-    fn write(&self , text: &str) -> String {
-        format!(
-            "\x1b[38;2;{};{};{}m{}\x1b[0m",
-            self.value().0,
-            self.value().1,
-            self.value().2,
-            text
-        )
-    }
 }
+
+struct Palette {
+    red: Terminal,
+    blue: Terminal,
+}
+
+const PALETTE: Palette = Palette {
+    red: Color(255, 65, 54),
+    blue: Color(0, 191, 255),
+};
+
 
 fn main() {
-   print_tree();
+    print_tree();
 }
 
-fn print_tree(){
-
+fn print_tree() {
     const HEIGHT: i32 = 30;
     let mut j = 1;
     let mut k = 0;
-    let space = " ";
     let star = "*";
+    let ball = "O";
+
+    print!("{}",Clear().format(""));
 
     //green
 
     for i in 1..HEIGHT {
-        while j <= HEIGHT-i {
-            j += 1;
-            print!(" ")
-        }
-        j = 1;
-        while k != 2*i-1 {
-            print!("\x1b[38;5;46m*");
-            k += 1;
-        }
-        k = 0;
-        println!();
-    }
-
-
-    //trunk
-    for i in 1..HEIGHT/5 {
-        while j <= HEIGHT-i {
+        while j <= HEIGHT - i {
             j += 1;
             print!(" ");
         }
         j = 1;
-        while k != 2*i-1 {
-            print!("\x1b[38;5;100m*");
+        while k != 2 * i - 1 {
+            print!("{}", PALETTE.blue.format(star));
             k += 1;
         }
         k = 0;
         println!();
     }
 
+    //trunk
+    for i in 3..HEIGHT / 4 {
+        while j <= HEIGHT - i {
+            j += 1;
+            print!(" ");
+        }
+        j = 1;
+        while k != 2 * i - 1 {
+            print!("{}", PALETTE.blue.format(star));
+            k += 1;
+        }
+        k = 0;
+        println!();
+    }
 }
